1788ffc1e972541b5b82b238eaaafb549f3999e9
Tigran Petrosyan <tigr-080807@mail.ru>
2026-02-02 05:48:03 +0500

    Оптимизация моделей

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
+from .managers import GetOrNoneManager, IsDeletedManager
+from django.utils import timezone
+
 
 # Create your models here.
+class BaseModel(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = GetOrNoneManager()
+
+    class Meta:
+        abstract = True
+
+
+class IsDeletedModel(BaseModel):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = IsDeletedManager()
+
+    class Meta(BaseModel.Meta):
+        pass
+
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save(update_fields=["is_deleted", "deleted_at"])
+
+    def hard_delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
