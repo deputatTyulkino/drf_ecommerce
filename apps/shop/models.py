82d47044ce26342a79622f05566ecb0abc20f86d
Tigran Petrosyan <tigr-080807@mail.ru>
2026-02-03 01:04:04 +0500

    Создание моделей продавцов и магазина

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,41 @@
+from autoslug import AutoSlugField
 from django.db import models
 
+from apps.common.models import BaseModel, IsDeletedModel
+from apps.sellers.models import Seller
+
+
 # Create your models here.
+class Category(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from="name", always_update=True, unique=True)
+    image = models.ImageField(upload_to="category_images/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Product(IsDeletedModel):
+    seller = models.ForeignKey(
+        Seller, ob_delete=models.SET_NULL, null=True, related_name="products, "
+    )
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from="name", always_update=True)
+    desc = models.TextField()
+    price_old = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    price_current = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+
+    is_stock = models.IntegerField(default=5)
+
+    image1 = models.ImageField(upload_to="product_images/")
+    image2 = models.ImageField(upload_to="product_images/", blank=True)
+    image3 = models.ImageField(upload_to="product_images/", blank=True)
+
+    def __str__(self):
+        return self.name
